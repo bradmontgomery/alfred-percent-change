f9ee56cab7ddf6e7241d2b54a7c709a181305073
Brad Montgomery <brad@bradmontgomery.net>
2014-08-16 14:03:27 -0500

    renaming things

--- script.py
+++ script.py
@@ -16,7 +16,7 @@ def percent_of(a, b):
     return str(round((a / b) * 100, 2))
 
 
-def calculate_percent_change(a, b):
+def percent_change(a, b):
     """Given two floats, calculate the percent change and return a rounded
     string representation of the change."""
     if a > 0:
@@ -31,7 +31,7 @@ def parse(input_string):
     values = input_string.strip().split(' ')
     if len(values) == 2:
         # We got `% a b`, calculate percent change.
-        return calculate_percent_change(float(values[0]), float(values[1]))
+        return percent_change(float(values[0]), float(values[1]))
     elif len(values) == 3 and 'of' in values:
         return percent_of(float(values[0]), float(values[-1]))
 
@@ -62,14 +62,14 @@ def test_percent_of():
     _eq(percent_of(5.0, 3.0), "166.67")
 
 
-def test_calculate_percent_change():
-    _eq(calculate_percent_change(3.0, 6.0), "100.0")
-    _eq(calculate_percent_change(5.0, 2.0), "-60.0")
-    _eq(calculate_percent_change(3.5, 3.52), "0.57")
+def test_percent_change():
+    _eq(percent_change(3.0, 6.0), "100.0")
+    _eq(percent_change(5.0, 2.0), "-60.0")
+    _eq(percent_change(3.5, 3.52), "0.57")
 
 
 def test_parse():
-    _eq(parse("3 6"), "100.0")  # calculate_percent_change
+    _eq(parse("3 6"), "100.0")  # percent_change
     _eq(parse("2 of 5"), "40.0")  # percent_of
 
 
@@ -79,7 +79,7 @@ if __name__ == "__main__":
 
     elif len(sys.argv) == 2 and sys.argv[1] == "test":
         print("Running Tests:\n")
-        test_calculate_percent_change()
+        test_percent_change()
         test_percent_of()
         print("\n\nDone.")
         if len(FAILURES):
